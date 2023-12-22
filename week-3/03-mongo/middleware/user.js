50894f2a766dc417f89f02231fecb3870119777e
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-12-22 16:16:44 +0530

    typo corrected

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -2,9 +2,9 @@ const {User}=require('../db');
 
 async function userMiddleware(req, res, next) {
     const {username,password}=req.headers;
-    const VerifiedAdmin= await User.findOne({username:username,password:password});    
+    const VerifiedUser= await User.findOne({username:username,password:password});    
 
- if(!VerifiedAdmin){
+ if(!VerifiedUser){
     res.status(401).send("Unauthorized");
     return;
  }else{
